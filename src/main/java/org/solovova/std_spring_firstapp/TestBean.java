045ef25fce9ce355777786e0c56b9127121a0ba8
Solovova <vsolo0@gmail.com>
2021-03-02 08:04:33 +0200

    0.01 Bean sample

---
+++ src/main/java/org/solovova/std_spring_firstapp/TestBean.java
@@ -0,0 +1,17 @@
+package org.solovova.std_spring_firstapp;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
